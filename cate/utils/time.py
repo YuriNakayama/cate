bf1a646b4487b6fe25aa618ab8c98299bca4195d
YuriNakayama <yuri620620@gmail.com>
2024-10-23 02:06:59 +0000

    feat: update timer

--- cate/utils/time.py
+++ cate/utils/time.py
@@ -9,8 +9,10 @@ from typing import Literal
 @dataclass
 class Event:
     time: datetime
-    name: str
+    model: str
+    phase: str
     category: Literal["start", "stop"]
+    description: str = ""
 
     @classmethod
     def keys(cls) -> dict_keys:
@@ -21,16 +23,17 @@ class Timer:
     def __init__(self) -> None:
         self.events: list[Event] = []
 
-    def start(self, name: str) -> None:
-        event = Event(datetime.now(), name, "start")
+    def start(self, model: str, phase: str, description: str | None = None) -> None:
+        description = "" if description is None else description
+        event = Event(datetime.now(), model, phase, "start", description)
         self.events.append(event)
 
-    def stop(self, name: str) -> None:
-        event = Event(datetime.now(), name, "stop")
+    def stop(self, model: str, phase: str, description: str | None = None) -> None:
+        description = "" if description is None else description
+        event = Event(datetime.now(), model, phase, "stop", description)
         self.events.append(event)
 
     def to_csv(self, path: Path) -> None:
         with path.open(mode="w", encoding="utf-8", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=list(Event.keys()))
             writer.writerows([asdict(event) for event in self.events])
-    
