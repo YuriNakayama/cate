8cb9d1278e6e90088b5f4c89e5948a4adfac04c5
YuriNakayama <yuri620620@gmail.com>
2024-10-26 10:38:34 +0000

    Add new metrics classes for uplift modeling

---
+++ cate/metrics.py
@@ -0,0 +1,24 @@
+
+class Auuc:
+    pass
+
+class UpliftByPercentile:
+    """
+    https://statisticsschool.com/%E3%80%90%E5%9B%A0%E6%9E%9C%E6%8E%A8%E8%AB%96%E3%80%91uplift-modeling%E3%82%A2%E3%83%83%E3%83%97%E3%83%AA%E3%83%95%E3%83%88%E3%83%A2%E3%83%87%E3%83%AA%E3%83%B3%E3%82%B0%E3%81%AB%E3%81%A4%E3%81%84/
+    スコア上位k%までのユーザーに対するUplift(tgのcv率-cgのcv率)を計算する
+    """
+    pass
+
+class QiniCoefficient:
+    """
+    https://www.jstage.jst.go.jp/article/pjsai/JSAI2020/0/JSAI2020_1H4OS12b02/_pdf
+    """
+
+class QiniCurve:
+    """
+    https://www.jstage.jst.go.jp/article/pjsai/JSAI2020/0/JSAI2020_1H4OS12b02/_pdf
+    """
+    pass
+
+class UpliftCurve:
+    pass
