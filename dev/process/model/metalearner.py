0bb2fccdbf31ed9dfcf967b8bebdf9b826243ebf
YuriNakayama <yuri620620@gmail.com>
2024-11-05 13:05:39 +0000

    Refactor metalearner to improve MLflow integration and streamline metrics logging

--- dev/process/model/metalearner.py
+++ dev/process/model/metalearner.py
@@ -11,10 +11,10 @@ from causalml.inference.meta import (
 from sklearn.model_selection import StratifiedKFold
 from tqdm import tqdm
 
-from cate.model.dataset import Dataset, to_rank
+from cate.infra.mlflow import MlflowClient
+from cate.model.dataset import Dataset
 from cate.model.evaluate import Auuc, QiniByPercentile, UpliftByPercentile, UpliftCurve
 from cate.model.metrics import Artifacts, Metrics
-from cate.infra.mlflow import MlflowClient
 from cate.utils import Timer, get_logger, path_linker
 
 dataset_name = "test"
@@ -22,8 +22,6 @@ logger = get_logger("causalml")
 pathlinker = path_linker(dataset_name)
 client = MlflowClient(dataset_name)
 timer = Timer()
-
-client.start_run(tags={"models": "metalearner", "dataset": dataset_name})
 
 logger.info("load dataset")
 ds = Dataset.load(pathlinker.base)
@@ -45,14 +43,13 @@ models = {
 
 np.int = int  # type: ignore
 
-metrics = Metrics([Auuc(), UpliftByPercentile(0.1), QiniByPercentile(0.1)])
-artifacts = Artifacts([UpliftCurve()])
-
-pred_dfs = {}
 skf = StratifiedKFold(5, shuffle=True, random_state=42)
 for name, model in models.items():
-    _pred_dfs = []
     logger.info(f"start {name}")
+    client.start_run(
+        tags={"model": name, "dataset": dataset_name, "package": "causalml"}
+    )
+    _pred_dfs = []
     for i, (train_idx, valid_idx) in tqdm(
         enumerate(skf.split(np.zeros(len(ds)), ds.y))
     ):
@@ -75,30 +72,21 @@ for name, model in models.items():
         timer.start(name, "predict", i)
         pred = model.predict(valid_X, p=np.full(valid_X.shape[0], train_w.mean()))
         timer.stop(name, "predict", i)
-
-        artifacts(pred.reshape(-1), valid_y, valid_w)
+        metrics = Metrics([Auuc(), UpliftByPercentile(0.1), QiniByPercentile(0.1)])
         metrics(pred.reshape(-1), valid_y, valid_w)
+        client.log_metrics(metrics, i)
 
         _pred_dfs.append(
             pd.DataFrame({"index": ds.y.index[valid_idx], "pred": pred.reshape(-1)})
         )
-    client.log_metrics(metrics)
-    artifacts.clear()
+
+    pred_df = pd.concat(_pred_dfs, axis=0)
+    base_df = pd.merge(ds.y.copy(), ds.w.copy(), left_index=True, right_index=True)
+    output_df = pd.merge(base_df, pred_df, left_index=True, right_index=True)
+
+    artifacts = Artifacts([UpliftCurve()])
+    artifacts(output_df.pred.to_numpy(), output_df.y.to_numpy(), output_df.w.to_numpy())
     client.log_artifacts(artifacts)
-    metrics.clear()
-    pred_dfs[name] = _pred_dfs
+    client.end_run()
 
-output_df = pd.merge(ds.y.copy(), ds.w.copy(), left_index=True, right_index=True)
-for name, pred_df_list in pred_dfs.items():
-    pred_df = pd.concat(pred_df_list, axis=0)
-    rank = to_rank(pred_df["index"], pred_df["pred"], ascending=False)
-    pred_df = pd.merge(pred_df, rank, left_on="index", right_index=True).set_index(
-        "index", drop=True
-    )
-    pred_df = pred_df.rename(columns={"pred": f"{name}_pred", "rank": f"{name}_rank"})
-    output_df = pd.merge(output_df, pred_df, left_index=True, right_index=True)
-
-output_df.to_csv(pathlinker.prediction / "metaleaner.csv")
 timer.to_csv(pathlinker.prediction / "metalearner_duration.csv")
-
-client.end_run()
