fdb3cce068db79e4802c08bf395aa954f78171ad
YuriNakayama <yuri620620@gmail.com>
2024-12-15 08:25:21 +0000

    fix: Ensure logger handlers are properly closed before adding new handler

--- cate/utils/log.py
+++ cate/utils/log.py
@@ -12,6 +12,9 @@ def get_logger(name: str, level: int = INFO) -> Logger:
         "%(levelname)s  %(asctime)s [%(name)s] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
+    for h in logger.handlers[:]:
+        logger.removeHandler(h)
+        h.close()
     std_handler = StreamHandler(sys.stdout)
     std_handler.setFormatter(formatter)
     logger.addHandler(std_handler)
