caa95da3de008681e683868ca7fb0ba60c6869ed
YuriNakayama <yuri620620@gmail.com>
2024-12-15 10:14:00 +0000

    feat: Add random_ratio parameter to model execution for enhanced data variability

--- process/model/biased_data/run.sh
+++ process/model/biased_data/run.sh
@@ -1,4 +1,5 @@
 python process/model/biased_data \
     --multirun \
     data.name=hillstorm,megafon,lenta,criteo \
-    model.name=drlearner,xlearner,rlearner,slearner,tlearner
+    model.name=drlearner,xlearner,rlearner,slearner,tlearner \
+    data.random_ratio=0.0,0.2,0.4,0.6,0.8,1.0
