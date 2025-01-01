60d480b4ca48f2cd88da367d8e2d431a4ee69426
YuriNakayama <yuri620620@gmail.com>
2025-01-01 19:38:25 +0900

    :white_check_mark: Improve debug output in test_save_creates_files by converting glob results to a list

--- tests/dataset/test_dataset.py
+++ tests/dataset/test_dataset.py
@@ -84,7 +84,7 @@ def test_save_creates_files(sample_dataset: cds.Dataset, tmp_path: Path) -> None
     sample_dataset.save(save_path)
 
     assert (save_path / "data.parquet").exists()
-    assert (save_path / "meta.db").exists(), save_path.glob("*")
+    assert (save_path / "meta.db").exists(), list(save_path.glob("*"))
 
 
 def test_save_overwrites_existing_directory(
